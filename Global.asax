ba274efcb229be55cd85b267c463f4c1c81f7705
SharkyIX <89183770+SharkyIX@users.noreply.github.com>
2021-09-15 13:13:01 +0200

    Initial commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_7409Summative.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_7409Summative.Startup))]
+namespace _7409Summative
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
